37eb88fc0784876f5633905367a299e14deca841
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 12:51:49 -0400

    ok now doing the indent right lets seee

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
-      
         if ord(char) >= 97 and ord(char) <= 122:
-           
-            char = chr(ord(char) - 32)
-          
-            print("{}".format(char), end="")
-        print("")
+                char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print("")
