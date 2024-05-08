a0f8d72adaa35620b67ede214066422563a7848d
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 10:11:46 -0400

    jsut changing some simple thing jajaj with help of chatgpt

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = abs(number) % 10
-
-print("Last digit of", number, "is", last, end=" ")
+last = number % 10 if number >= 0 else ((number * -1) % 10) * -1
 
 if last > 5:
-    print("and is greater than 5")
-
-elif last == 0:
-    print("and is 0")
+    output = 'Last digit of {} is {} and is greater than 5'.format(number, last)
 
 elif last < 6 and last != 0:
-    if number < 0:
-        print("and is less than 6 and not 0")
-    else:
-        print("and is less than 6 and not 0")
+    output = 'Last digit of {} is {} and is less than 6 and not 0'.format(number, last)
+
+elif last == 0:
+    output = 'Last digit of {} is {} and is 0'.format(number, last)
+
+print(output)
