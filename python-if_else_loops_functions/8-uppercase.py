58fcb142d913427598ca324ae604401f1b5c18c5
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 12:16:14 -0400

    anothe one

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,6 +5,6 @@ def uppercase(str):
         # Check if character is a lowercase letter
         if 97 <= assci_value <= 122:
             # Convert lowercase letter to uppercase by subtracting 32 from Ascii
-            print("{} ".format(chr(assci_value - 32)), end="")
+            print("{}".format(chr(assci_value - 32)), end=" ")
         else:
             print(char, end="")
