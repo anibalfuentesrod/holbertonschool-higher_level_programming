82f9c462187bdfdd1a45deb87c7e87a508b3c9ac
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 12:48:01 -0400

    againnngggggggg

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,6 +6,6 @@ def uppercase(str):
            
             char = chr(ord(char) - 32)
           
-            print("{}".format(char), end=" ")
+            print("{}".format(char), end="")
             
-    print("")
+    print(" ")
