ae7c0e95bc4ab56a7f2eebb129300cfc880383b9
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 12:52:23 -0400

    deleteing the over indented on the line 5

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for char in str:
         if ord(char) >= 97 and ord(char) <= 122:
-                char = chr(ord(char) - 32)
+            char = chr(ord(char) - 32)
         print("{}".format(char), end="")
     print("")
