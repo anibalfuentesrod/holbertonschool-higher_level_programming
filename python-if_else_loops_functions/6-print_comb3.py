0d469676d8f6958213f03693a9559f29613bceeb
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 11:46:57 -0400

    deleting the extra comma tha 89 has

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for tens_digit in range(0, 9):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:02d}".format(tens_digit * 10 + ones_digit), end=", ")
+        print("{:02d}".format(tens_digit * 10 + ones_digit), end="")
+
+        if tens_digit != 8 or ones_digit != 9:
+            print(", ", end="")
