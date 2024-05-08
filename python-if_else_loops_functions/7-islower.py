81143e7be1858d3a6d18c3efc2b1490480c93315
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 11:57:59 -0400

    pyhton i love you you are so intelligent jajaj

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def islower(c):
+    assci_value = ord(c)
+    return 97 <= assci_value <= 122
+
+print("a is {}".format("lower" if islower("a") else "upper"))
+print("H is {}".format("lower" if islower("H") else "upper"))
+print("A is {}".format("lower" if islower("A") else "upper"))
+print("3 is {}".format("lower" if islower("3") else "upper"))
+print("g is {}".format("lower" if islower("g") else "upper"))
