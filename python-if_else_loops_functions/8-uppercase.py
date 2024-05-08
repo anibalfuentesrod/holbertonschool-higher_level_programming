ef3f22a0d5153ad013f88bef16e853bb4eec7366
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 12:21:00 -0400

    changing 97 to 96

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,9 @@ def uppercase(str):
     for char in str:
         assci_value = ord(char)
         # Check if character is a lowercase letter
-        if 97 <= assci_value <= 122:
+        if 96 <= assci_value <= 122:
             # Convert lowercase letter to uppercase by subtracting 32 from Ascii
             print("{}".format(chr(assci_value - 32)), end="")
         else:
             print(char, end="")
+uppercase("holberton")
