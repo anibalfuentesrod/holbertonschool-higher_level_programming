e6b787d145794f49545f0fb9c19c1763008c9e61
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 10:22:35 -0400

    jajajja a string complete made of {} ajajaj this bullsh***

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 last = number % 10 if number >= 0 else ((number * -1) % 10) * -1
 prefix = "Last digit of"
+str = "less than 6 and not 0"
 if last > 5:
     output = "{} {} is {} and is greater than 5".format(prefix, number, last)
 
 elif last < 6 and last != 0:
-    output = ("{} {} is {} and is less than 6 "
-    "and not 0".format(prefix, number, last))
+    output = "{} {} is {} {}".format(prefix, number, last, str)
 
 elif last == 0:
     output = "{} {} is {} and is 0".format(prefix, number, last)
