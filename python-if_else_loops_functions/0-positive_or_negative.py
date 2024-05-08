1c54bca4ff7c718c83edc07f20a30d83b62b5fdf
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 08:55:19 -0400

    we dont have betty but we got pycodestyle helll noooo !!!

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,11 +2,10 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(number,"is positive")
+    print(number, "is positive")
 
 elif  number == 0:
-    print(number,"is zero")
+    print(number, "is zero")
 
 elif number < 0:
-    print(number,"is negative")
-    
+    print(number, "is negative")
