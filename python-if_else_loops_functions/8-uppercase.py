4744834ac04ef2c3b688f3ac07e82e4366c09678
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 12:08:54 -0400

    jajja let's gooo but its tricky that i don;t have to use main anymore jajaj

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        assci_value = ord(char)
+        # Check if character is a lowercase letter
+        if 97 <= assci_value <= 122:
+            # Convert lowercase letter to uppercase by subtracting 32 from Ascii
+            print("{}".format(chr(assci_value - 32)), end="")
+        else:
+            print(char, end="")
+    print()
