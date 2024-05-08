36ec9b5237335a16cb19332b41b1bbe4a8451189
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-08 10:25:11 -0400

    i was making the string bad jajaj "and is less" that was missing the "and is" jajaja

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last = number % 10 if number >= 0 else ((number * -1) % 10) * -1
 prefix = "Last digit of"
-str = "less than 6 and not 0"
+str = "and is less than 6 and not 0"
 if last > 5:
     output = "{} {} is {} and is greater than 5".format(prefix, number, last)
 
