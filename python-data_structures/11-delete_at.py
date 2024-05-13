cd0c119a1f46f3a404fb9ec1d67f478b16e6c95c
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-13 13:16:35 -0400

    me estab complicando con este ejercicio y chatgpt me ayudo jajaj

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,8 +2,6 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    new_list = []
-    for i in range(len(my_list)):
-        if i != idx:
-            new_list.append(my_list[i])  
-    return new_list
+    
+    del my_list[idx]
+    return my_list
