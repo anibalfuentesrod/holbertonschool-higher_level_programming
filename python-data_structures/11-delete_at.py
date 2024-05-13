2eab43d60ea4f0197ae1fe325216512e25c6b8dc
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-13 12:54:50 -0400

    let's see, i know there is somening wrong but let's see

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+    
+    new_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+    return new_list
