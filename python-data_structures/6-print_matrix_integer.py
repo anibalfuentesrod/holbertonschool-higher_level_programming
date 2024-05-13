e38ebafa35147a82d5d76d1093f5b7735d0c376a
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-13 13:23:16 -0400

    pushing the 6 with the help of chatgpt

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for num in row:
-            print("{:d}".format(num), end=" ")
-        print()
+    for row_idx in range(len(matrix)):
+        row_length = len(matrix[row_idx])
+        for col_idx in range(row_length):
+            if col_idx != row_length - 1:
+                end_char = ' '
+            else:
+                end_char = ''
+            print("{:d}".format(matrix[row_idx][col_idx]), end=end_char)
+        print("")
