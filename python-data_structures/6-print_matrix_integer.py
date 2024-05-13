47bad03506d024f8d5bae7ea7546dc637c8f871b
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-13 11:10:54 -0400

    moving forward the for loop

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
+        
         for num in row:
             print("{:d}".format(num), end=" ")
         print()
