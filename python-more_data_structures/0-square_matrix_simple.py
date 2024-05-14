a68786d8c6a445808a4a50f6af4494ffc001ec63
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-14 08:34:01 -0400

    pushing the 0 and pycodestyle too

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+
+    for row in matrix:
+        new_row = []
+
+        for element in row:
+            new_row.append(element * element)
+
+        new_matrix.append(new_row)
+    return new_matrix
