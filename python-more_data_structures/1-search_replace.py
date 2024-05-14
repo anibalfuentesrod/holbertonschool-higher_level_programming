51c73ecfe65cd3ce5fd9e2165bc1467fae1ac78d
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-14 09:21:14 -0400

    making the 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = []
+
+    for item in my_list:
+
+        if item == search:
+            new_list.append(replace)
+        else:
+            new_list.append(item)
+    return new_list
