934e5b027fbbcf124bd30e817b45e276a09f2176
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-15 10:51:31 -0400

    pushing the ajjjajjj adding the count += 1 in the fricking code i am acustic

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             if type(my_list[i]) is int and count != x:
                 print("{:d}".format(my_list[i]), end='')
+                count += 1
         except TypeError:
             continue
 
