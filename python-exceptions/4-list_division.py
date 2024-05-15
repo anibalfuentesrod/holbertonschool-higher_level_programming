fe41c628d5a689888571984c3ec442a0e817f14e
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-15 10:33:01 -0400

    ahahhhhhhhhhh pushing agoinnnnn

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,18 +3,16 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-            if i >= len(my_list_1) or i >= len(my_list_2):
-                raise IndexError("out of range")
             numerator = my_list_1[i]
             denominator = my_list_2[i]
             try:
-                result.append(float(numerator) / float(denominator))
+                result.append(numerator / denominator)
             except ZeroDivisionError:
                 print("division by 0")
                 result.append(0)
-            except ValueError:
-                print("wrong type")
-                result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
         except IndexError:
             print("out of range")
             result.append(0)
