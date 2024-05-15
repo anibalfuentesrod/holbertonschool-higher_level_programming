171502c9f44c9cb38096d34dea82c221c61c590b
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-15 10:41:26 -0400

    pushing the f*** code jajj let's gooo it was an identetion

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@ def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
         try:
-           result.append(my_list_1[i] / my_list_2[i])
+            result.append(my_list_1[i] / my_list_2[i])
         except ZeroDivisionError:
             result.append(0)
             print("division by 0")
