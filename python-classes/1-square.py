b5198ef52fd0966e2016b7756cb20ca57a836a26
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-17 11:14:32 -0400

    pushign the 1, where i need to make a private attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+
+This is a class call Square, with a private attribute
+
+"""
+
+
+class Square:
+    """
+
+     A class to respresent square
+
+     This class has a private instance attribute call size
+
+    """
+    def __init__(self, size):
+        """
+
+        Initialize the Square with a private size atributte
+
+        Args:
+            size: The size of the square
+
+        """
+        self.__size = size
