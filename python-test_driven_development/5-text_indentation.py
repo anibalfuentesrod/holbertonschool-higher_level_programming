0835f0c1a727b30a2c1a43c5de8cccf876916fd8
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-20 16:26:28 -0400

    pushing

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def text_indentation(text):
     """Prints a text with 2 new line after this char
-    
+
     Args:
         text (str): the text to print
     Raises:
@@ -9,12 +9,11 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text musssssst be a string")
-    
-    
+
     char = 0
     while char < len(text) and text[char] == ' ':
         char += 1
-    
+
     while char < len(text):
         print(text[char], end="")
         if text[char] == "\n" or text[char] in ".?:":
