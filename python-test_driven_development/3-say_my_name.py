0569eaab784e643021b48607746b1612be8ee601
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-20 13:18:48 -0400

    ahkfhdwkljfkakfdlkjfkdjfja,jf4

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 def say_my_name(first_name, last_name=""):
     """
-    Prints "My name is <first name> <last name>
+    Prints "My name is <first name> <last name>.
 
     Args:
-        first_name (str): first name
-        las_name (str): last name and default ""
+        first_name (str): The first name.
+        last_name (str): The last name. Default is an empty string.
 
     Raises:
-        TypeError: If first_name or last_name is not a string
+        TypeError: If first_name or last_name is not a string.
     """
-    if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
 
-    if type(last_name) is not str:
-        raise TypeError('last_name must be a string')
-
-    print('My name is', first_name, last_name)
+    if last_name:
+        print("My name is {} {}".format(first_name, last_name))
+    else:
+        print("My name is {}".format(first_name))
