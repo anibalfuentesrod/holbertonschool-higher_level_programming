98ac0abf4c347a878bdad94bee39617da9c9de7b
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-20 14:35:04 -0400

    i just cant it's so much look at this thing

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """Prints a text with 2 new line after this char
+    
+    Args:
+        text (str): the text to print
+    Raises:
+        TypeError: if tezt is not str
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be as string")
+    
+    characters = [".", "?", ":"]
+    result = ""
+    i = 0
+
+    while i < len(text):
+        result += text[i]
+        if text[i] in characters:
+            result += "\n\n"
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+                continue
+        i += 1
+    print(result.strip())
