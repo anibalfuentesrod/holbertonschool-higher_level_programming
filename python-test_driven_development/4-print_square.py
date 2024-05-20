d99309c9e3c9b245e5dbb234889e4ae66e459151
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-20 14:22:09 -0400

    pycodestyle

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_square(size):
     """Print a square with the char #
-    
+
     Args:
         size (int): THe size len of the square.
     Raises:
@@ -12,6 +12,6 @@ def print_square(size):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-    
+
     for _ in range(size):
         print("#" * size)
