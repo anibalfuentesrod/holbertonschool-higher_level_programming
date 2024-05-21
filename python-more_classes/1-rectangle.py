4b6d99f58bc3b19fe63978cd7eacdeefbe650d41
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-21 11:40:58 -0400

    pushing the first one 1-rectangle

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Making a class: Rectange that defines a rectangle
+"""
+
+
+class Rectangle:
+    """
+    def some functions, like, height and width
+
+    Raises:
+        TypeError
+        ValueError
+    """
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__heigh = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width mus be >= 0")
+        self.__width = value
