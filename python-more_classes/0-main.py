e847a3971278456c69eea985b09a2ec0f6d403eb
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-21 10:44:00 -0400

    pushing agian with the documentatio coments

--- python-more_classes/0-main.py
+++ python-more_classes/0-main.py
@@ -3,3 +3,4 @@ Rectangle = __import__('0-rectangle').Rectangle
 
 my_rectangle = Rectangle()
 print(type(my_rectangle))
+print(my_rectangle.__dict__)
--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+"""
+just an empty class
+"""
+
+
 class Rectangle:
     pass
