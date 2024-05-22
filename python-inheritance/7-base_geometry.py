815fe3686f06b0ad6736c3d68c168fe35d2eabee
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-22 12:45:16 -0400

    pushing allot of thing

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -20,6 +20,6 @@ class BaseGeometry:
             ValueError
         """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value < 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
