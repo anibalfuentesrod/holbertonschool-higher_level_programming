ab2638ef5b496e644a56e38173a625b392cb92df
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-22 11:32:53 -0400

    pushign agian

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -9,4 +9,7 @@ def is_same_class(obj, a_class):
     """
     my func returns the type() of obj adn compares it to a_class
     """
-    return type(obj) == a_class
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
