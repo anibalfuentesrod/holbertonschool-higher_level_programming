46b45e08f2ffdd3ab61f8de33f41aba809415879
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-22 14:45:07 -0400

    uususu

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -13,7 +13,12 @@ making a new inherited class call square
 class Square(Rectangle):
     """A class representing a square."""
     def __init__(self, size):
-        """Initialize the Square with size."""
+        """
+        Initialize the Square with size.
+        Args:
+            size
+            size
+        """
         super().__init__(size, size)
 
     def __str__(self):
