4ea14bec0ed1924e6dd5aae55ce6eda5ea0a8e8b
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-22 11:30:20 -0400

    pushing the 2 one

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Writing a func that returs true if the object is exactly
+an instance of the specified class
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    my func returns the type() of obj adn compares it to a_class
+    """
+    return type(obj) == a_class
