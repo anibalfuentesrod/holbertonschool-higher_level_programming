cf00d7a82ef83b2cbcd48eccce30297daec6b892
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-22 13:30:35 -0400

    pushing the 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,15 +5,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """This class inherits BaseGeometry"""
     def __init__(self, width, height):
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-
-    def __str__(self):
-        """Returns a string representation of the rectangle"""
-        return f"[Rectangle] {self.__width}/{self.__height}"
-
-    def __repr__(self):
-        """Returns a string representation of the object"""
-        return f"<Rectangle width={self.__width}, height={self.__height}>"
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
