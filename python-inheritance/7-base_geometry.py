28e2e4a929b49b58d3942bf6874d0e5c84d97549
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-22 13:28:58 -0400

    pushing the 7 and 8 let's see

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
 """
-Making a class with methods for area and int validation
+Makes sure the the value is int
 """
 
 
 class BaseGeometry:
-    """Raises an exception indicating that area ins not implemented"""
+    """
+    Class name
+    """
+
     def area(self):
-        raise Exception("area() is not implemented")
+        """
+        Raises an exception
+        """
+
+        raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        """Validates that the value is positive
-
+        """
         Args:
             name
             value
+
         Raises:
             TypeError
             ValueError
         """
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value < 0:
-            raise ValueError("{} must be greater than 0".format(name))
+
+        if type(value) is not int:
+            raise TypeError(name + ' must be an integer')
+
+        if value <= 0:
+            raise ValueError(name + ' must be greater than 0')
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,6 +7,13 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
-
         self.__width = width
         self.__height = height
+
+    def __str__(self):
+        """Returns a string representation of the rectangle"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        """Returns a string representation of the object"""
+        return f"<Rectangle width={self.__width}, height={self.__height}>"
