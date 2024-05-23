03e9ade416c8d06fde7394859abfe5f2772e7758
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-22 20:50:50 -0400

    changing the comment comiilas

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-'''Creating module BaseGeometry.'''
+"""Creating module BaseGeometry."""
 
 
 class BaseGeometry:
-    '''New BaseGeometry class.'''
+    """New BaseGeometry class."""
     def area(self):
-        ''' The method to raise an exception'''
+        """The method to raise an exception"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
-        '''Code to take the value'''
+        """Code to take the value"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
