06648d4427615741a96ab1241c92a6788ec936fa
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-24 13:02:23 -0400

    push

---
+++ python-abc/main_04_flyingfish.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+from task_04_flyingfish import Fish, FlyingFish
+
+flying_fish = FlyingFish()
+flying_fish.swim()
+flying_fish.fly()
+flying_fish.habitat()
---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,39 @@
+"""
+Creating multiple classes that inherits
+"""
+
+
+class Fish:
+    """
+    ......A class representing a fish
+    """
+    def swim(self):
+        print("The fish is swimming")
+
+    def habitad(self):
+        print("The fish lives in water")
+
+
+class Bird:
+    """
+    Class that represents a bird duhh
+    """
+    def fly(self):
+        print("The bird is flying")
+
+    def habitat(self):
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """
+    This class represents a flying fish, inherited both Fish and Bird
+    """
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def fly(self):
+        print("The flying fish is soaring")
+
+    def habitad(self):
+        print("The flying fish lives in both in water and in the sky!")
