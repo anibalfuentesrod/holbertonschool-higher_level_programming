89cb806611f21e5459ceb8157206e5a9434dcace
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 10:58:23 -0400

    the documentation!!

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import json
-"""Writing a function that returns the JSON representation of an obj"""
+"""
+Writing a function that returns the JSON representation of an obj
+"""
 
 
 def to_json_string(my_obj):
