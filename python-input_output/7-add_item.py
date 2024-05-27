ccd6898364874c597bcf1e3decb56263c2f689b4
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 12:03:30 -0400

    changing the name of var to meet the correct requeremnts of pycodestyle of the file 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Writing a scrip that adds all arguments to a py list and then save it
+"""
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json = __import__('5-save_to_json_file.py').save_to_json_file
+    load_from_json = __import__('6-load_from_json_file.py').load_from_json_file
+
+    try:
+        item = load_from_json("add_item.json")
+    except FileNotFoundError:
+        item = []
+    item.extend(sys.argv[1:])
+    save_to_json(item, "add_item.json")
