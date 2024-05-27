a518e222ffea5f6b1d4c6095be3cb66d2b44d072
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 11:13:37 -0400

    pushing the 5 jajajj let's goo

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Writing a function that writes an Object to a text file, using JSON"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Using the built-in function dump()"""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(my_obj, f)
