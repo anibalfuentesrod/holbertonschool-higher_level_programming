82142a625c98f3f69936fceb220ade20d5ce9e0b
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 10:22:48 -0400

    pushing socumntation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,7 @@ Writing a function that reads a txt file
 
 
 def read_file(filename=""):
+    """This function takes a filename and i opened with the func open()
+    """
     with open(filename, encoding='utf-8') as f:
         print(f.read())
