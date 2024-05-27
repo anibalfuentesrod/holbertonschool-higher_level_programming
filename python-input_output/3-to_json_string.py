1bf1bf00d43a024488eaef23ea7eb5a1385379f3
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 10:57:33 -0400

    making the 3 one

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import json
+"""Writing a function that returns the JSON representation of an obj"""
+
+
+def to_json_string(my_obj):
+    """Using json.dumps, to meet the expectations"""
+    return json.dumps(my_obj)
