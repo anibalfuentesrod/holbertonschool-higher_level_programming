c6427379d55288405291140c6dc1a6091462f272
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 11:51:01 -0400

    delting the thing i just id

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,13 +5,6 @@ import json
 
 def load_from_json_file(filename):
     """Using the built-in function load()"""
-    try:
-        with open(filename, 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            return data
-    except FileNotFoundError:
-        print("[FileNotFoundError] No such file or directory: '{}'".format(filename))
-    except PermissionError:
-        print("[PermissionError] Permission denied: '{}'".format(filename))
-    except json.JSONDecodeError as e:
-        print("[JSONDecodeError] {}".format(e))
+    with open(filename, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+        return data
