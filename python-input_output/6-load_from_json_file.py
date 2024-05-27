115b9fb96e71cf6530f8b9b5818c9f260f012951
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 11:49:30 -0400

    making som changes

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -5,5 +5,13 @@ import json
 
 def load_from_json_file(filename):
     """Using the built-in function load()"""
-    with open(filename, 'r', encoding='utf-8') as f:
-        return json.load(f)
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+            return data
+    except FileNotFoundError:
+        print("[FileNotFoundError] No such file or directory: '{}'".format(filename))
+    except PermissionError:
+        print("[PermissionError] Permission denied: '{}'".format(filename))
+    except json.JSONDecodeError as e:
+        print("[JSONDecodeError] {}".format(e))
