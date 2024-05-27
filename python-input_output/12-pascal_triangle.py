a3853a98e6ebcea51756e946e4be806909053a97
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 12:50:06 -0400

    pushing the last one

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This func returns a list of int"""
+
+
+def pascal_triangle(n):
+    """Returns a list"""
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
