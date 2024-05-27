571d4919ae5e4460ca66b6d1519511e6a0689605
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 11:24:41 -0400

    deleting the utf-8

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,10 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Using the built-in function dump()"""
-    try:
-        with open(filename, 'w', encoding='utf-8') as f:
-            json.dump(my_obj, f)
-    except PermissionError as pe:
-        print(f"[PermissionEroor] {pe}")
-    except TypeError as te:
-        print(f"[TypeErronr] {te}")
+    with open(filename, 'w') as f:
+        json.dump(my_obj, f)
