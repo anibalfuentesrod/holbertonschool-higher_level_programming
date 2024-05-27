64fb339eda360051b8247a4be5f3e25a2fb94704
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 10:37:26 -0400

    pushing the documentaiton

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,7 +6,8 @@ This function writes a string to .txt file
 
 def write_file(filename="", text=""):
     """
-
+    In this function i make a code that return the number of char
+    it has
     """
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
