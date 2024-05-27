0bbca7c828cad0507ff1b72e53684610c050a26a
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 12:05:30 -0400

    push4

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     save_to_json = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json = __import__('6-load_from_json_file.py').load_from_json_file
+    load_from_json = __import__('6-load_from_json_file').load_from_json_file
 
     try:
         item = load_from_json("add_item.json")
