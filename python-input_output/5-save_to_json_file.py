5962bb90be04578dbfaf45c53ac68a983236bd76
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-27 11:14:18 -0400

    deleting the permission

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Using the built-in function dump()"""
-    with open(filename, 'w', encoding='utf-8') as f:
+    with open(filename, encoding='utf-8') as f:
         json.dump(my_obj, f)
