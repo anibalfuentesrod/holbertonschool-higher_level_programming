72c951e8c6a835d8c3cbce785534b448b66e92d8
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-29 10:42:29 -0400

    pushing the code again, just changing names of var, and see if it works

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,13 +5,13 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     """This func is to serialize the dict = data, and save on filename"""
-    with open(filename, 'w') as j_file:
-        json.dump(data, j_file)
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
 
 
 def load_and_deserialize(filename):
     """Loads the json data in filename and desirialize into a dict"""
 
-    with open(filename, 'r') as j_file:
-        data = json.load(j_file)
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
     return data
