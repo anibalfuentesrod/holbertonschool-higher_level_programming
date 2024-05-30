d75ba29a842bc57863b797f2e8e1522a55483679
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-05-30 11:59:01 -0400

    pushin the 3

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""simple server"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    """comment"""
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_response('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+        elif self.path == '/data':
+            self.send_response(200)
+            self.send_header('Content-type', 'aplication/json')
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/status':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"OK")
+        else:
+            self.send_response(404)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"Endpoint not fount")
+
+
+def run():
+    server_address = ('', 8000)
+    http = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print('Starting server...')
+    http.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
