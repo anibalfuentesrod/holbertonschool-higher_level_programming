7462b510fa7f963762936481305594831a54a026
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-06-01 22:02:52 -0400

    chatgpts help thx ahhh

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,41 +1,65 @@
 #!/usr/bin/python3
-""""""
-from flask import Flask, jsonify, request
+"""adding a comment is boring"""
+
+from flask import Flask
+from flask import jsonify, request, abort
+
 
 app = Flask(__name__)
 
-users = {"jane": {"username": "jane", "name": "Jane", "age": 28, "city": "Los Angeles"},     
-         "john": {"username": "john", "name": "John", "age": 30, "city": "New York"}}
+users = {}
 
-@app.route('/')
+@app.route("/")
 def home():
+    """prints welcome at the start"""
     return "Welcome to the Flask API!"
 
-@app.route('/data')
-def get_usernames():
+@app.route("/data")
+def data():
+    """just returns data of json"""
     return jsonify(list(users.keys()))
 
-@app.route('/status')
-def get_status():
+@app.route("/status")
+def status():
+    """status of user"""
     return "OK"
 
-@app.route('/users/<username>')
-def get_user(username):
-    if username in users:
-        return jsonify(users[username])
-    else:
-        return "User not found", 404
-    
-@app.route('/add_user', methods=['POST'])
+@app.route("/users/<username>")
+def users_specific(username):
+    """Gets he usr specifiec if not prints usr not founc"""
+    if username not in users:
+        return jsonify({"error": "User not found"}), 404
+
+    output = users[username]
+    output["username"] = username
+
+    return jsonify(output)
+
+@app.route("/add_user", methods=["POST"])
 def add_user():
-    data = request.get_json()
-    if 'username' in data:
-        username = data['username']
-        users[username] = data
-        return jsonify({"message": "User added", "user": data})
-    else:
-        return "Missing username in request", 400
+    """ adds a new user to the dict """
+    if request.get_json() is None:
+        abort(400, "Not a JSON")
+
+    req_data = request.get_json()
+
+    if "username" not in req_data:
+        return jsonify({"error": "Username is required"}), 400
+
+    users[req_data["username"]] = {
+        "name": req_data["name"],
+        "age": req_data["age"],
+        "city": req_data["city"]
+    }
+
+    output = {
+        "username": req_data["username"],
+        "name": req_data["name"],
+        "age": req_data["age"],
+        "city": req_data["city"]
+    }
+    return jsonify({"message": "User added", "user": output}), 201
+
 
 if __name__ == "__main__":
-    # Run the Flask app
-    app.run(debug=True)
+    app.run(host='localhost', port=5000, debug=True)
