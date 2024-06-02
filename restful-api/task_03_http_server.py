93bc504a4eaa6b97b2be1dd77aaff8edff4b02e8
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-06-01 21:58:48 -0400

    pushing the 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -5,7 +5,7 @@ import json
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    """comment"""
+    """http request"""
     def do_GET(self):
         if self.path == '/':
             self.send_response(200)
@@ -18,6 +18,12 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.end_headers()
             data = {"name": "John", "age": 30, "city": "New York"}
             self.wfile.write(json.dumps(data).encode())
+        elif self.path == '/info':
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.end_headers()
+            data = {"version": "1.0", "description": "A simple API built with http.server"}
+            self.wfile.write(json.dumps(data).encode()) 
         elif self.path == '/status':
             self.send_response(200)
             self.send_header('Content-type', 'text/plain')
@@ -27,7 +33,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            self.wfile.write(b"Endpoint not fount")
+            self.wfile.write(b"404 Not Found")
 
 
 def run():
