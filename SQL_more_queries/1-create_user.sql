70cbc3db872dd7f2bb636c0366735fd7427017a8
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-06-18 10:10:35 -0400

    ahhhh pushing again i have to drop the user first, is what chatgpt is telling me hahahh

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,5 @@
 -- creating a user and granting privileges, the user have a password too
+DROP USER IF EXISTS 'user_0d_1'@'localhost';
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
