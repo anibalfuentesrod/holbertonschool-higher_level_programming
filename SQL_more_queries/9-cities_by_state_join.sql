0f3102b95809a4d6c685103918fb080e0009e477
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-06-18 12:07:25 -0400

    changing to alias becouse the code confused itself with the same names

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all cities along the corresponding state names
-SELECT cities.id, cities.name, states.name
+SELECT cities.id, cities.name AS city_name, states.name AS state_name
 FROM cities
-JOIN states ON cities.state_id = state_id
+JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC;
