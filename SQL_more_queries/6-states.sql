2a98ad6dc2503106a990980df23deece7af7a308
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-06-18 11:20:57 -0400

    pushing task 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- new database hbtn... and inside table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
