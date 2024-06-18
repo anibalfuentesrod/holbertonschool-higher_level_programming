c237a11cb4563c2e55848da65053b1d055b12ee0
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-06-18 12:12:38 -0400

    psuhign again

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list all cities along the corresponding state names
-SELECT cities.id, cities.name AS city_name, states.name AS state_name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id ASC;
+
+SELECT c.id, c.name AS city_name, s.name AS state_name 
+FROM cities AS c INNER JOIN states AS s ON c.state_id = s.id 
+ORDER BY c.id;
