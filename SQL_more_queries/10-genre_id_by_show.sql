bdb0afce75a0fc5a2c2d25096964f01164ef27ed
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-06-18 12:24:57 -0400

    ahhh pushing the task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- importing data to my server, and the script list all showz contained in it
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
