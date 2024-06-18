b2d4a297e103c8c2353e78a550600356f87f7a64
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-06-18 12:41:56 -0400

    oushing tha last excercise !!!!! let's gooo !!!

---
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,6 @@
+-- list all shows and all genres linked to that show from datbase htb...tvshow
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
