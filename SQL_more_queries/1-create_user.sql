f804e7c6c9e5eca80478db4b6ca77caab80459dd
An1b4l <anibalfuentesrodriguez@gmail.com>
2024-06-18 09:57:35 -0400

    pushing the task 2

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- creating a user and granting privileges, the user have a password too
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
