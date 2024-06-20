92b754194aef374b0e625f64c1c870d6e1311b10
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-06-20 13:19:59 -0400

    changing the if main thing to be executed

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,34 +2,31 @@
 import MySQLdb
 import sys
 
-if __name__ == "__main__":
-    # get mysql credentials and database name from command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
+def list_states(username, password, dbname):
+    # connects to mysql database
+    db = MySQLdb.connect(host="localhost", user=username, password=password, db=dbname)
 
-    # connecting to server...
-    db = MySQLdb.connect(
-    host='localhost',
-    port=3306,
-    user=mysql_username,
-    passwd=mysql_password,
-    db=database_name
-    )
-
-    # cursor object to interact with the database
+    # create a cursor object
     cursor = db.cursor()
 
-    # execute the sql query to list all states in asc order by id
-    cursor.execute("SELECT id, name FROM states ORDER BY id ASC")
+    # execute the sql query
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
 
-    # fetch all the rows returned by the query
+    # fetch all the rows
     rows = cursor.fetchall()
 
     # print each row
     for row in rows:
         print(row)
-    
-    # close the cursor and database
+
     cursor.close()
     db.close()
+
+if __name__ == "__main__":
+    # get command line arg
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    # call the function to list states
+    list_states(username, password, dbname)
