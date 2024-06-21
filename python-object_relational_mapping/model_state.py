8a0bab25c3b66cc17dd3a085219ce4f4c5133d3c
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-06-21 11:50:49 -0400

    making the 6 and adding the other files

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# create base class using the function declarative_base()
+Base = declarative_base()
+
+class State(Base):
+    """states class that links to 'states' in mysql server"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
