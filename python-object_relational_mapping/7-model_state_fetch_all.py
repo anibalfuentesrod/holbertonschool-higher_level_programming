1ed3f5c0ac0d5c411fed2d0fc3d291a2485e687b
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-06-21 12:18:15 -0400

    making the 7 and pycodestyle too

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""list states in database hbt..."""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    # get mysql username, password, and database name
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    # create engine to connect to mysql server
+    engine = create_engine(f'mysql+mysqldb://{username}: \
+                           {password}@localhost:3306/ \
+                            {dbname}', pool_pre_ping=True)
+
+    # create a config session class
+    Session = sessionmaker(bind=engine)
+
+    # now create the session
+    session = Session()
+
+    # query all states in order by id
+    states = session.query(State).order_by(State.id).all()
+
+    # print each state using a for loop
+    for state in states:
+        print(f"{state.id}: {state.name}")
+
+    # close the session
+    session.close()
---
+++ python-object_relational_mapping/7-model_state_fetch_all.sql
@@ -0,0 +1,7 @@
+-- Insert states
+INSERT INTO states (name) VALUES (
+    "California"), 
+    ("Arizona"), 
+    ("Texas"), 
+    ("New York"), 
+    ("Nevada");
