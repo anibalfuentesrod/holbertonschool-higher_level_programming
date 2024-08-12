0bd590278b3f0237de8803e22a6e7644eb9092c9
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-08-12 18:18:59 -0400

    making task 2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""puttting annotations for the functionnnn"""
+import math
+
+
+def floor(n: float) -> int:
+    """return the largest integer less than or equal to n using built-in"""
+    return math.floor(n)
