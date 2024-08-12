a5263f348ab37073ef990957475efc7ca1bdc30b
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-08-12 18:36:40 -0400

    built-in funciton sum()

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""annotation to lists now"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """returns the sum of the list"""
+    return sum(input_list)
