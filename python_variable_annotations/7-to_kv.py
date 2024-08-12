17abae7e3fdde1f477930ea685ff4f07bd5c6708
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-08-12 19:31:24 -0400

    adding documentation

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -4,4 +4,5 @@ from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """reutrn the a tuple  that takes str and floats"""
     return (k, float(v ** 2))
