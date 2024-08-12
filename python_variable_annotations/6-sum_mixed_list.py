e77ad0e35c46bc29bad71e0eb270da0c5667705e
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-08-12 18:42:20 -0400

    sum of list of floats and ints

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""same thing mannnnn"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int]) -> float:
+    """return the sum of floats and int and returns on floats"""
+    return sum(mxd_lst)
