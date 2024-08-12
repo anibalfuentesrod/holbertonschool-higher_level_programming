f77ad5cc62d1e37b945fe2ffbc0102ac11975455
Anibal <157289919+anibalfuentesrod@users.noreply.github.com>
2024-08-12 18:10:32 -0400

    making task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Same, making annotations to function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns the concat of the two strings"""
+    return str1 + str2
